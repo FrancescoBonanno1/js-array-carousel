fa5123ce96212e42bb37eb3eed1fc2e31c81a3ab
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-11-02 16:53:17 +0100

    Problema sul classlist

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-const imagePath=[ "img\02.webp" , "img\01.webp" , "img\03.webp",  "img\04.webp", "img\05.webp"];
-console.log(imagePath);
+imagePath=[ "img/02.webp" , "img/01.webp" , "img/03.webp",  "img/04.webp", "img/05.webp"];
+
+for (let i = 0; i < imagePath.length; i++) {
+    const image= imagePath[i];
+    image.classlist.add("active");
+}
